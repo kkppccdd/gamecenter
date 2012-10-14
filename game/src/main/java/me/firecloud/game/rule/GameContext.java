116d963b2adec2b3099d0a35129296273ac9aa61
kkppccdd <kkppccdd@gmail.com>
2012-10-14 20:00:51 +0800

    modify something

--- game/src/main/java/me/firecloud/game/rule/GameContext.java
+++ game/src/main/java/me/firecloud/game/rule/GameContext.java
@@ -3,10 +3,16 @@
  */
 package me.firecloud.game.rule;
 
+import java.util.List;
+
 /**
  * @date 2012-10-12
- *
+ * 
  */
 public interface GameContext {
+	public Object get(String epxression);
 
+	public List<Object> getList(String expression);
+
+	public GameContext set(String expression, Object value);
 }
